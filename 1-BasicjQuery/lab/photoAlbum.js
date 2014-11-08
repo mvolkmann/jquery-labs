f2f70c6381c096b63c76599cd640dcbfdf1139ef
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2014-11-08 13:54:03 -0600

    fixes to lab #1

--- 1-BasicjQuery/lab/photoAlbum.js
+++ 1-BasicjQuery/lab/photoAlbum.js
@@ -1,63 +1,65 @@
-/*global $: false, alert: false */
-'use strict';
+(function () {
+  /*global $: false, alert: false */
+  'use strict';
 
-var pa = {};
+  var deleteButton, descField, urlField;
 
-// Adds a row to the photo table for the entered URL and description.
-pa.addPhoto = function () {
-  var desc, url;
+  // Adds a row to the photo table for the entered URL and description.
+  function addPhoto() {
+    var desc, url;
 
-  url = pa.urlField.val();
-  desc = pa.descField.val();
-  if (url && desc) {
-    pa.addPhotoRow(desc, url);
-  } else {
-    alert("URL and description are required.");
+    url = urlField.val();
+    desc = descField.val();
+    if (url && desc) {
+      addPhotoRow(desc, url);
+    } else {
+      alert("URL and description are required.");
+    }
   }
-};
 
-// Adds a row to the photo table for the given URL and description.
-pa.addPhotoRow = function (desc, url) {
-  var img, td, tr;
+  // Adds a row to the photo table for the given URL and description.
+  function addPhotoRow(desc, url) {
+    var img, td, tr;
 
-  tr = $('<tr>');
-  tr.append('<td><input type="checkbox"/></td>');
-  img = $('<img>', { src: url });
-  img.addClass('photo');
-  td = $('<td>');
-  td.append(img);
-  tr.append(td);
-  tr.append('<td>' + desc + '</td>');
-  $('#photoTable').append(tr);
-};
+    tr = $('<tr>');
+    tr.append('<td><input type="checkbox"/></td>');
+    img = $('<img>', { src: url });
+    img.addClass('photo');
+    td = $('<td>');
+    td.append(img);
+    tr.append(td);
+    tr.append('<td>' + desc + '</td>');
+    $('#photoTable').append(tr);
+  }
 
-// Deletes all the selected rows in the photo table.
-pa.deletePhotos = function () {
-  $('#photoTable :checkbox:checked').parent().parent().remove();
-  pa.deleteButton.attr('disabled', true);
-};
+  // Deletes all the selected rows in the photo table.
+  function deletePhotos() {
+    $('#photoTable :checkbox:checked').parent().parent().remove();
+    deleteButton.attr('disabled', true);
+  }
 
-// Enables/disables the "Delete" button
-// based on whether any rows are selected.
-pa.evaluateCheckboxes = function () {
-  var checked = $('#photoTable :checkbox:checked');
-  pa.deleteButton.attr('disabled', checked.size() === 0);
-};
+  // Enables/disables the "Delete" button
+  // based on whether any rows are selected.
+  function evaluateCheckboxes() {
+    var checked = $('#photoTable :checkbox:checked');
+    deleteButton.attr('disabled', checked.size() === 0);
+  }
 
-// Performs page setup, including event handler registration.
-pa.setup = function () {
-  $('h1').css('color', 'red');
+  // Performs page setup, including event handler registration.
+  function setup() {
+    $('h1').css('color', 'red');
 
-  pa.urlField = $('#url');
-  pa.descField = $('#desc');
+    urlField = $('#url');
+    descField = $('#desc');
 
-  $('#add').click(pa.addPhoto);
+    $('#add').click(addPhoto);
 
-  pa.deleteButton = $('#delete');
-  pa.deleteButton.attr('disabled', true);
-  pa.deleteButton.click(pa.deletePhotos);
+    deleteButton = $('#delete');
+    deleteButton.attr('disabled', true);
+    deleteButton.click(deletePhotos);
 
-  $('#photoTable').on('change', ':checkbox', pa.evaluateCheckboxes);
-};
+    $('#photoTable').on('change', ':checkbox', evaluateCheckboxes);
+  }
 
-$(pa.setup);
+  $(setup);
+})();
